dc6edb88d8d54f7eca4f2b02003f3d6541af9593
bc-andreadao <andrea.dao@bigcommerce.com>
2025-10-15 11:25:28 -0700

    update styling, prettify

--- app/page.js
+++ app/page.js
@@ -6,21 +6,19 @@ import defaultProducts from "@/src/product-data";
 
 async function fetchProductList() {
   const products = await new Promise((resolve) =>
-    setTimeout(() => resolve(defaultProducts), 5000)
+    setTimeout(() => resolve(defaultProducts), 3000)
   );
 
-  return <ProductList products={products} />
+  return <ProductList products={products} />;
 }
 
 export default function Home() {
-
-
   return (
-    <div className="flex flex-col items-center max-w-7xl mx-auto px-4">
-      <h1 className="text-3xl text-center mb-8">Products</h1>
+    <>
+      <h1 className="m-8">Products</h1>
       <Suspense fallback={<div>Loading products...</div>}>
         {fetchProductList()}
       </Suspense>
-    </div>
+    </>
   );
 }
