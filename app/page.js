ee1eb019a839da972594248667fa1fd364148d0b
bc-andreadao <andrea.dao@bigcommerce.com>
2025-10-17 19:09:48 -0700

    add suspense boundaries for separate data fetches

--- app/page.js
+++ app/page.js
@@ -4,9 +4,9 @@ import { ProductList } from "@/vibes/soul/sections/product-list";
 
 import defaultProducts from "@/src/product-data";
 
-async function fetchProductList() {
+async function fetchProductList(delay) {
   const products = await new Promise((resolve) =>
-    setTimeout(() => resolve(defaultProducts), 3000)
+    setTimeout(() => resolve(defaultProducts), delay)
   );
 
   return <ProductList products={products} />;
@@ -17,7 +17,11 @@ export default function Home() {
     <>
       <h1 className="text-3xl m-8">Products</h1>
       <Suspense fallback={<div>Loading products...</div>}>
-        {fetchProductList()}
+        {fetchProductList(2000)}
+      </Suspense>
+
+      <Suspense fallback={<div>Loading products...</div>}>
+        {fetchProductList(5000)}
       </Suspense>
     </>
   );
