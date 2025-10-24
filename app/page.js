7233432e2ad7854ee97af6605256c87bed4d9a31
bc-andreadao <andrea.dao@bigcommerce.com>
2025-10-23 23:58:44 -0700

    page component returns static data

--- app/page.js
+++ app/page.js
@@ -3,14 +3,11 @@ import { ProductList } from "@/vibes/soul/sections/product-list";
 import defaultProducts from "@/src/product-data";
 
 export default async function Home() {
-  const products = await new Promise((resolve) =>
-    setTimeout(() => resolve(defaultProducts), 3000)
-  );
 
   return (
     <>
       <h1 className="text-3xl m-8">Products</h1>
-      <ProductList products={products} />
+      <ProductList products={defaultProducts} />
     </>
   );
 }
